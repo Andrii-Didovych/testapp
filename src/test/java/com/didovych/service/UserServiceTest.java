522b96cca8ee121e84ed7e1ec72ebf2e4469ff62
Andrii-Didovych <andrii.didovych@sombrainc.com>
2019-09-11 17:39:35 +0300

    created simple application with tests

--- src/test/java/com/didovych/service/UserServiceTest.java
+++ src/test/java/com/didovych/service/UserServiceTest.java
@@ -12,6 +12,7 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -20,6 +21,7 @@ import java.util.Collections;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UserServiceTest {
+
     @Autowired
     private UserService userService;
 
@@ -36,13 +38,15 @@ public class UserServiceTest {
     public void addUser() {
         User user = new User();
 
-        user.setEmail("some@mail.ru");
+        user.setEmail("some@mail");
+        user.setPassword("111");
 
         boolean isUserCreated = userService.addUser(user);
 
         Assert.assertTrue(isUserCreated);
         Assert.assertNotNull(user.getActivationCode());
         Assert.assertTrue(CoreMatchers.is(user.getRoles()).matches(Collections.singleton(Role.USER)));
+        Assert.assertTrue(CoreMatchers.is(passwordEncoder.encode("111")).matches(user.getPassword()));
 
         Mockito.verify(userRepo, Mockito.times(1)).save(user);
         Mockito.verify(mailSender, Mockito.times(1))
@@ -101,5 +105,83 @@ public class UserServiceTest {
         Assert.assertFalse(isUserActivated);
 
         Mockito.verify(userRepo, Mockito.times(0)).save(ArgumentMatchers.any(User.class));
+    }
+
+    @Test
+    public void loadUserByUsernameTest() {
+
+        Mockito.doReturn(new User())
+                .when(userRepo)
+                .findByUsername("John");
+
+        User john = (User) userService.loadUserByUsername("John");
+
+        Assert.assertNotNull(john);
+    }
+
+    @Test(expected = UsernameNotFoundException.class)
+    public void loadUserByUsernameFailTest() {
+        userService.loadUserByUsername("Ross");
+    }
+
+
+    @Test
+    public void updateEmailTest() {
+        String newEmail = "another@email";
+        User user = new User();
+        user.setEmail("current@mail");
+
+        userService.updateProfile(user, "", newEmail);
+
+        Assert.assertNotNull(user.getActivationCode());
+        Assert.assertTrue(CoreMatchers.is(user.getEmail()).matches("another@email"));
+
+        Mockito.verify(userRepo, Mockito.times(1)).save(user);
+
+        Mockito.verify(mailSender, Mockito.times(1)).send(
+                ArgumentMatchers.matches(user.getEmail()),
+                ArgumentMatchers.anyString(),
+                ArgumentMatchers.anyString()
+        );
+    }
+
+    @Test
+    public void updateEmailIfTheSameEnteredTest() {
+        String newEmail = "current@mail";
+        User user = new User();
+        user.setEmail("current@mail");
+
+        userService.updateProfile(user, null, newEmail);
+
+        Assert.assertNull(user.getActivationCode());
+
+        Mockito.verify(userRepo, Mockito.times(1)).save(user);
+        Mockito.verify(mailSender, Mockito.times(0)).send(
+                ArgumentMatchers.anyString(),
+                ArgumentMatchers.anyString(),
+                ArgumentMatchers.anyString()
+        );
+    }
+
+    @Test
+    public void changePasswordOnly() {
+        String newPassword = "000";
+        User user = new User();
+        user.setPassword("111");
+        userService.updateProfile(user, newPassword, null);
+
+        Assert.assertNull(user.getActivationCode());
+        Assert.assertTrue(CoreMatchers.is(user.getPassword())
+                .matches(passwordEncoder.encode(newPassword)));
+
+
+        Mockito.verify(userRepo, Mockito.times(1)).save(user);
+
+        Mockito.verify(mailSender, Mockito.times(0)).send(
+                ArgumentMatchers.anyString(),
+                ArgumentMatchers.anyString(),
+                ArgumentMatchers.anyString()
+
+        );
     }
 }
